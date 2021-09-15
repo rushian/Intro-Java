ef63f8a12b0fb9b22c9f2798efc54fed4bad5fdd
Luciano <luciano.ivec@gmail.com>
2021-09-15 12:14:22 -0300

    Adiccao dos modos

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -3,29 +3,31 @@ package intro;
 
 // 2 - Referencia as bibliotecas
 
-
-import java.util.Arrays;
+//import java.util.Arrays;
 
 // 3 - Classe
 public class Medidas {
     // 3.1 Atributos - caracteristicas
-
-
-
+    static int largura;
+    static int comprimento;
     // 3.2 Metodos e funcoes
     public static void main (String [] args) {
-        System.out.println("Calculo de areas");
-        //Calculo de area
-        int largura;
-        int comprimento;
-
         largura = 5;
         comprimento = 6;
 
-        calcularAreaModoCompacto(largura, comprimento);
+        calcularAreaModoCurto(largura, comprimento);
+        calcularAreaModoExtenso(largura, comprimento);
     }
-    public static void calcularAreaModoCompacto(int largura, int comprimento){
+    public static void calcularAreaModoCurto(int largura, int comprimento){
+        System.out.println("Calculo de areas");
         System.out.println("A área é de " + largura * comprimento +
+                " m² ("+ largura +" x "+comprimento+")" );
+    }
+    public static void calcularAreaModoExtenso(int largura, int comprimento){
+        System.out.println("Calculo de areas");
+        int resultado;
+        resultado = largura * comprimento;
+        System.out.println("A área é de " + resultado +
                 " m² ("+ largura +" x "+comprimento+")" );
     }
 }
